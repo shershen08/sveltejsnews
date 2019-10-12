45d534b981f29743e8fc5b2c1e28f470c72ace4a
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-10-12 22:09:05 +0200

    fix mobile layout

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@
   // Init Wow
   wow = new WOW({
     animateClass: 'animated',
-    offset: 100
+    offset: 0
   });
   wow.init();
 
@@ -28,13 +28,13 @@
 
 
   //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 50) {
-      $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+  // $(window).scroll(function() {
+  //   if ($(".navbar-default").offset().top > 50) {
+  //     $(".navbar-fixed-top").addClass("top-nav-collapse");
+  //   } else {
+  //     $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  //   }
 
-  });
+  // });
 
 })(jQuery);
